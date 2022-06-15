2beb6f56d0b24271903524e0a78857f10fb18bec
ChathuraLiyanapathirana <cbuddhika3@gmail.com>
2022-06-16 00:18:00 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
-
-const StopwatchOne = () => {
-  const [timer, setTimer] = useState(0);
-
-  // console.log('Watch one rendering!');
-
-  useEffect(() => {
-    setInterval(() => {
-      setTimer((value) => value + 1);
-    }, 1000);
-  }, []);
-
-  return <span>{timer}</span>;
-};
-
-const StopwatchTwo = () => {
-  const [timer, setTimer] = useState(0);
-
-  // console.log('Watch one rendering!');
-
-  useEffect(() => {
-    const timerInterval = setInterval(() => {
-      setTimer((value) => value + 1);
-    }, 1000);
-    return () => clearInterval(timerInterval);
-  }, []);
-
-  return <span>{timer}</span>;
-};
+import WatchExample from './WatchExample';
 
 export default function App() {
   return (
     <div>
-      <h1>Strict Mode Example</h1>
-      <div>
-        Watch One : <StopwatchOne />
-      </div>
-      <div>
-        Watch One : <StopwatchTwo />
-      </div>
+      {/* Can you to identify the memory leaks */}
+      <WatchExample />
     </div>
   );
 }
