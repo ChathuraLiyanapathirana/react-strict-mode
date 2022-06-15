d879693ef6beae0ea3701bf91ee14aeda49998a4
ChathuraLiyanapathirana <cbuddhika3@gmail.com>
2022-06-16 00:10:38 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+const StopwatchOne = () => {
+  const [timer, setTimer] = useState(0);
+
+  // console.log('Watch one rendering!');
+
+  useEffect(() => {
+    setInterval(() => {
+      setTimer((value) => value + 1);
+    }, 1000);
+  }, []);
+
+  return <span>{timer}</span>;
+};
+
+const StopwatchTwo = () => {
+  const [timer, setTimer] = useState(0);
+
+  // console.log('Watch one rendering!');
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      setTimer((value) => value + 1);
+    }, 1000);
+    return () => clearInterval(timerInterval);
+  }, []);
+
+  return <span>{timer}</span>;
+};
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Strict Mode Example</h1>
+      <div>
+        Watch One : <StopwatchOne />
+      </div>
+      <div>
+        Watch One : <StopwatchTwo />
+      </div>
     </div>
   );
 }
